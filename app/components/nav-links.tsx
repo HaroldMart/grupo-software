f2f7dc0e5484712f0d08a695a0e371f33ebb8f8d
Isael Diroche <isaeldiroche00@gmail.com>
2024-03-01 22:01:18 -0400

    feat: featuring a new navigation bar

---
+++ app/components/nav-links.tsx
@@ -0,0 +1,52 @@
+'use client'
+
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+import clsx from 'clsx';
+
+const links = [
+    { 
+        name: 'Home', 
+        href: '/',
+    },
+    {
+        name: 'About Us',
+        href: '/about-us',
+    },
+    {
+        name: 'Services',
+        href: '/services',
+    },
+    { 
+        name: 'Our Team', 
+        href: '/our-team',    
+    },
+    { 
+        name: 'Contact', 
+        href: '/contact',     
+    },
+];
+
+export default function NavLinks() {
+    const pathname = usePathname();
+    return (
+      <>
+        {links.map((link) => {
+          return (
+            <Link
+              key={link.name}
+              href={link.href}
+              className={clsx(
+                'link',
+                {
+                  'active': pathname === link.href,
+                },
+              )}
+            >
+              <p>{link.name}</p>
+            </Link>
+          );
+        })}
+      </>
+    );
+  }
