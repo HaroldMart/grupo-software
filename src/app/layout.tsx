8f49e3ba0ffbef565e3e4574254718bdb32dff89
Isael Diroche <isaeldiroche00@gmail.com>
2024-03-08 01:02:36 -0400

    styles: real content in home
    Cambie el texto de pruebas por texto real

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,9 @@ import "../components/static/css/globals.scss";
 import { inter } from "../utils/fonts";
 import { Footer } from "../components/partials/footer";
 import Navbar from "../components/partials/navbar";
+import { IoMdMail } from "react-icons/io";
+import { FaPhone } from "react-icons/fa6";
+
 
 export const metadata: Metadata = {
 	title: "Grupo Software",
@@ -17,6 +20,13 @@ export default function RootLayout({
 	return (
 		<html lang="en">
 			<body className={inter.className}>
+				<div className="top-navigation">
+					<p className="adress">Al. Brucknera Aleksandra 63, Wrocław 51-410</p>
+					<div className="phone-mail">
+						<div className="phone"><FaPhone className="icon" /> <p>+1 809 678 1819</p></div>
+						<div className="mail"><IoMdMail className="icon" /> <p>gruposoftware@gmail.com</p></div>
+					</div>
+				</div>
 				<Navbar />
 
 				{children}
