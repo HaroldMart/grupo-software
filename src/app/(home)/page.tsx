8c556c1ac5f452031162e683337d4129165119d3
Isael Diroche <isaeldiroche00@gmail.com>
2024-03-12 17:16:08 -0400

    fix: merge from "feat/home-page-structure"

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -9,6 +9,7 @@ import { BiStoreAlt } from "react-icons/bi";
 import { BsBoxSeam } from "react-icons/bs";
 import { BsBarChart } from "react-icons/bs";
 import { BsBook } from "react-icons/bs";
+import { ImQuotesLeft } from "react-icons/im";
 
 import AboutImagen from '../../../public/images/about-image.png'
 import GroupSvg from '../../../public/images/undraw_programming_re_kg9v.svg'
@@ -20,10 +21,10 @@ export default function Page() {
 			<div className="hero-section">
 				<div className="hero-description">
 					<div className="hero-tagline">
-						<span>New</span> Lorem ipsum dolor sit amet consectetur adipisicing elit.
+						<span>Turning</span> Visionary Ideas into Powerful Solutions
 					</div>
-					<h1>Find your <span>course</span> & make sure goal.</h1>
-					<p>Your dream Courses is waithing for you. 👋</p>
+					<h1>Welcome to <span>GrupoSoftware</span></h1>
+					<p>Your hub for digital solutions 👋</p>
 
 					<div className="hero-buttons">
 						<a href="#" className="filled">get started</a>
@@ -41,17 +42,16 @@ export default function Page() {
 					<div>
 						<Image src={AboutImagen} alt="Imagen de about" />
 					</div>
-					
+
 					<div className="content">
 						<Headline
 							tagline="About us"
-							title="Travelling as a discovery and progress."
+							title="Creating exceptional digital experiences"
 						/>
 						<div>
-							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Enim nisi neque provident fuga voluptatum quo vel impedit cum, voluptas illo tempora! Aliquid ratione placeat vero esse. Minima nisi consequatur corrupti?</p>
-							<h5>Nuestra mision</h5>
-							<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis veniam doloremque eum, tempore numquam amet facilis aspernatur officia rem? Autem suscipit quod delectus repellat, fugit deserunt minima! Reprehenderit, iste molestiae!
-							Rem soluta voluptatem hic odit facilis corrupti laborum incidunt eligendi cumque iusto quibusdam cupiditate voluptatum similique recusandae illo, architecto earum quam repellat impedit harum voluptas aliquid. Iure deserunt quisquam exercitationem!</p>
+							<p>We are a digital agency focused on creating exceptional digital experiences for our clients. We specialize in web, mobile and desktop development, delivering innovative and high-quality solutions.</p>
+							<h5>Our approach </h5>
+							<p>Our approach is based on understanding each client's unique needs and delivering customized solutions that drive their online success. We strive to exceed our customers' expectations on every project, focusing on quality, innovation, and customer satisfaction</p>
 						</div>
 					</div>
 
@@ -60,44 +60,119 @@ export default function Page() {
 
 			</Section>
 
+			<div className="portfolio-cta">
+				<Headline title="Ready to get started?"
+					description="Contact us today and find out how we can help you take your digital presence to the next level."
+					alignment="center"
+				/>
+				<a href="#" className="btn filled">Get Started</a>
+			</div>
+
 			<Section>
 				<Headline
-					tagline="About us"
-					title="Travelling as a discovery and progress."
-					description="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda hic minima quam officiis, nam nobis incidunt. Modi nam, necessitatibus accusamus esse officia ratione provident enim eum ipsa! Consectetur, eos officia."
+					tagline="Services"
+					title="Discover Our Comprehensive Range of Exceptional Services"
+					description="At GrupoSoftware, we offer a wide range of services to meet all your digital needs. Our team of experts is here to help you achieve your business goals and take your ideas to the next level. Whether you need web development, mobile app design, or technology consulting, we've got you covered."
 				/>
-					<ul className="content services-cont">
-						<li className="item">
-							<div className="item-content">
-								<div className="icon-cont primary">
-									<BsBook className="icon" size={28} fill="#9E64FF" />
-								</div>
-								<h4>How does writing influence your personal brand?</h4>
-								<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. doloremque odit quae totam ullam facere eveniet architecto!</p>
+				<ul className="content services-cont">
+					<li className="item">
+						<div className="item-content">
+							<div className="icon-cont primary">
+								<BsBook className="icon" size={28} fill="#9E64FF" />
 							</div>
-							<a className="btn" href="#">Get started <FaArrowRight /></a>
-						</li>
-						<li className="item">
-							<div className="item-content">
-								<div className="icon-cont secondary">
-									<BsBarChart className="icon" size={28} fill="#9E64FF" />
-								</div>
-								<h4>How does writing influence your personal brand?</h4>
-								<p>Lorem Commodi dolorum sint iste nihil eos, expedita magni, doloremque odit quae totam ullam facere eveniet architecto!</p>
+							<h4>Web Design Solutions</h4>
+							<p>Our design team creates stunning and functional websites that cater to the needs of your business.</p>
+						</div>
+						<a className="btn" href="#">Learn More <FaArrowRight /></a>
+					</li>
+					<li className="item">
+						<div className="item-content">
+							<div className="icon-cont secondary">
+								<BsBarChart className="icon" size={28} fill="#9E64FF" />
 							</div>
-							<a className="btn" href="#">Get started <FaArrowRight /></a>
-						</li>
-						<li className="item">
-							<div className="item-content">
-								<div className="icon-cont tertiary">
-									<BsBoxSeam className="icon" size={28} fill="#9E64FF" />
-								</div>
-								<h4>How does writing influence your personal brand?</h4>
-								<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Optio quia vero odio quo natus. </p>
+							<h4>Mobile App Development</h4>
+							<p>We develop custom mobile applications for iOS and Android that deliver an exceptional experience for your users.</p>
+						</div>
+						<a className="btn" href="#">Learn More <FaArrowRight /></a>
+					</li>
+					<li className="item">
+						<div className="item-content">
+							<div className="icon-cont tertiary">
+								<BsBoxSeam className="icon" size={28} fill="#9E64FF" />
 							</div>
-							<a className="btn" href="#">Get started <FaArrowRight fill="#69686E" /></a>
-						</li>
-					</ul>
+							<h4>Desktop Programming</h4>
+							<p>We offer robust and scalable desktop software solutions to meet the unique needs of your enterprise.</p>
+						</div>
+						<a className="btn" href="#">Learn More <FaArrowRight fill="#69686E" /></a>
+					</li>
+				</ul>
+			</Section>
+
+			<div className="services-cta">
+				<div className="content">
+					<Headline
+						title="Discover Our Diverse Portfolio of Innovative Projects"
+						description="Discover our latest projects and see how we've helped businesses like yours succeed."
+					/>
+					<a href="#" className="btn filled">View Portfolio</a>
+				</div>
+				<div className="image">
+					<img src="/images/project-image.png" alt="macbook image" />
+				</div>
+			</div>
+
+			<Section>
+			<Headline
+					tagline="Testimonials"
+					title="What Our Clients Say"
+					description="Explore the feedback from our valued clients and discover how our services have made a positive impact on their businesses. We take pride in delivering exceptional results and building long-lasting relationships with our clients."
+				/>
+				<ul className="testimonials-list">
+					<li className="item">
+						<div className="t-card-head">
+							<div className="person-info">
+								<img src="/images/contributors/jonathan pic.png" alt="jonathan picture" />
+								<div>
+									<h5>Jonathan Montero</h5>
+									<p>Founder, ABC Corporation</p>
+								</div>
+							</div>
+							<div className="rate-date">
+								<p>This week</p>
+								<p className="rate">⭐⭐⭐⭐⭐</p>
+							</div>
+						</div>
+
+						<div className="t-card-body">
+							<ImQuotesLeft size={32}  fill="#393644"/>
+							<div className="testimonial">
+								<p>GrupoSoftware delivered exactly what we needed for our project. Their expertise and professionalism were evident throughout the entire process. Highly recommended!</p>
+							</div>
+						</div>
+					</li>
+					<li className="item">
+						<div className="t-card-head">
+							<div className="person-info">
+								<img src="/images/contributors/isael pic.png" alt="jonathan picture" />
+								<div>
+									<h5>Isael Diroche</h5>
+									<p>CEO, Example Company</p>
+								</div>
+							</div>
+							<div className="rate-date">
+								<p>This week</p>
+								<p className="rate">⭐⭐⭐⭐⭐</p>
+							</div>
+						</div>
+
+						<div className="t-card-body">
+							<ImQuotesLeft size={32} fill="#393644"/>
+							<div className="testimonial">
+								<p>Working with GrupoSoftware has been a game-changer for our business. Their team went above and beyond to ensure our satisfaction and the results speak for themselves.</p>
+							</div>
+						</div>
+					</li>
+				</ul>
 			</Section>
 		</main>
 	);
